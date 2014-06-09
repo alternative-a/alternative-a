06bc3091af06fa2daa60894142bc7fa19f1a5f90
melimelo <melanie.eychenne@orange.fr>
2014-06-09 12:56:39 +0200

    Ajout de la page article_photoshop.php

---
+++ website/article_photoshop.php
@@ -0,0 +1,59 @@
+<article>
+	<h3> <a href="http://www.photoshop.com">Photoshop<a/> </h3>
+	<p>
+		Photoshop, le logiciel le plus connu pour le traitement d'images, possède un seul défault: son prix. Heureusement, il existe des logiciels gratuits permettant tout aussi bien de traiter des images.
+	</p>
+	
+	<h4> <a href="http://www.gimp.org">Gimp</a></h4>
+	<p>
+		Gimp est un logiciel gratuit et libre (open source). Il fonctionne sur les principaux systèmes d'exploitation dont Microsoft Windows, GNU/Linux et Mac.
+		<br>
+		GIMP possède des outils utilisés pour la retouche et l'édition d'image tels que:
+		<ul>
+			<li>le dessin à main levée</li>
+			<li>réajuster</li>
+			<li>rogner</li>
+			<li>photomontages</li>
+			<li>convertir entre différents formats d'image</li>
+			<li>les images animées comme les fichiers GIF et MPEG peuvent être créées en utilisant un plugin d'animation.
+			<li>et d'autres tâches plus spécialisées</li>
+		</ul>
+		Capture d'écran:
+		<br>
+		<img src="images/gimp_screenshot_windows.jpg" alt="gimp" title="Design de Gimp" />
+	</p>
+	
+	<h4> <a href="http://windows.microsoft.com/fr-fr/windows7/products/features/paint">Paint</a> </h4>
+	<p>
+		Paint est un logiciel livré avec toutes les versions de Windows. Il vous permet de faire des modifications très basiques sur vos images facilement.
+		<br>
+		Ces principales options dans la barre d'outils:
+		<ul>
+		<li>sélection libre</li>
+		<li>sélection rectangulaire</li>
+		<li>Redimensionner et incliner</li>
+		<li>gomme/gomme couleur</li>
+		<li>remplissage</li>
+		<li>prélèvement d'une couleur</li>
+		<li>zoom/Loupe</li>
+		<li>pinceau</li>
+		<li>brosse</li>
+		<li>aérographe</li>
+		<li>Pastel</li>
+		<li>texte</li>
+		<li>ligne</li>
+		<li>courbe</li>
+		<li>rectangle</li>
+		<li>polygone</li>
+		<li>ellipse</li>
+		<li>rectangle arrondi et autres formes géométriques</li>
+		</ul>
+	</p>
+	
+	<h5>Sources</h5>
+	<p>
+		<a href="http://fr.wikipedia.org/wiki/GIMP"></a>
+	</p>
+</article>
+
+
