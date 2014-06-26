8843c4041752d3dbd4c9f0536bfc6f068816e527
archer <archer.tb@hotmail.fr>
2014-06-26 14:59:44 +0200

    modification de article_moteur_de_recherches.php

--- website/articles/article_moteur_de_recherches.php
+++ website/articles/article_moteur_de_recherches.php
@@ -4,11 +4,13 @@
 		<a href="https://www.google.fr/">Google</a>, le moteur de recherche par défaut de Windows n'est pas le seul moteur de recherche.
 	</p>
 
+    <h4>remomandé</h4>
 	<h4> Duckduckgo </h4>
 	<p>
 		<a href="https://duckduckgo.com/">DuckDuckGo</a>, son slogan en dit long: "Google tracks you. We don't". La philosophie de ce moteur de recherche est de recherche est de préserver la vie privée et de ne stocker aucune information personnelle concernant les utilisateurs.
 	</p>
 
+    <h4>autre:</h4>
 	<h4> Yahoo! </h4>
 	<p>
 		<a href="https://fr.yahoo.com/">Yahoo!</a> est un moteur de recherche vous permettant de sélectionner et d'annoter les pages et sites web qui vous paraissent être vraiment intéressants (c'est le Social Search).
