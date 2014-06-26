a37370cf91671fc73091e0732294c6664c689eba
archer <archer.tb@hotmail.fr>
2014-06-26 14:54:58 +0200

    modification article_editeur_de_texte.php

--- website/articles/article_editeur_de_texte.php
+++ website/articles/article_editeur_de_texte.php
@@ -1,6 +1,7 @@
 ﻿<article>
 	<h3><strong>Alternative à Bloc note</strong></h3>
 
+    <h4>recomandé débutant:</h4>
 	<h4>Notepad++</h4>
 	<p>
 		<a href="http://notepad-plus-plus.org/fr/"> Notepad++ </a> est un éditeur de texte très utilisé. Il nous permet de travailler sur plusieurs documents en même temps et facilement grâce à la présence d'onglets. On peut également comparer la nouvelle version du document créé avec la dernière sauvegarde faite. Il est utilisable sous Windows uniquement.
@@ -8,21 +9,7 @@
 		Pour les développeurs, il y a la présence de la coloration syntaxique de code source pour les langages et fichiers C, C++, Java, C#, XML, HTML, PHP, JavaScript, makefile, art ASCII, doxygen, .bat, MS fichier ini, ASP, Visual Basic/VBScript, SQL, Objective-C, CSS, Pascal, Perl, Python, R, MATLAB, Lua, TCL, Assembleur, Ruby, Lisp, Scheme, Properties, Diff, Smalltalk, PostScript et VHDL ainsi que pour tout autre langage informatique, car ce logiciel propose la possibilité de créer ses propres colorations syntaxiques pour un langage quelconque.
 	</p>
 
-	<h4>VIM</h4>
-	<p>
-		<a href="http://doc.ubuntu-fr.org/vim"> VIM </a> est un éditeur de texte pour le terminal sous GNU/Linux. Malgré une mise en main difficile, il est utilisé par de nombreux programmeurs car il est très puissant.
-		<br>
-		<a href="http://www.vim.org/index.php"> Site internet officiel </a> (en anglais)
-	</p>
-
-	<h4>EMACS</h4>
-	<p>
-		<a href="http://doc.ubuntu-fr.org/emacs"> EMACS </a> est tout comme VIM un éditeur de texte avec une prise en main difficile mais tout de même de nombreux développeurs l'utilisent.
-		<br>
-		<a href="https://www.gnu.org/software/emacs/"> Site internet officiel </a> (en anglais)
-	</p>
-
-	<h4>Gedit</h4>
+    <h4>Gedit</h4>
 	<p>
 		<a href="http://doc.ubuntu-fr.org/gedit"> Gedit </a> est un éditeur de texte porté sous les systèmes d'UNIX, Windows (avec quelques bugs) et Mac.
 		<br>
@@ -43,14 +30,25 @@
 		</ul>
 	</p>
 
+    <h4>recomandé power user:</h4>
+	<h4>VIM</h4>
+	<p>
+		<a href="http://doc.ubuntu-fr.org/vim"> VIM </a> est un éditeur de texte pour le terminal sous GNU/Linux. Malgré une mise en main difficile, il est utilisé par de nombreux programmeurs car il est très puissant.
+		<br>
+		<a href="http://www.vim.org/index.php"> Site internet officiel </a> (en anglais)
+	</p>
+
+	<h4>EMACS</h4>
+	<p>
+		<a href="http://doc.ubuntu-fr.org/emacs"> EMACS </a> est tout comme VIM un éditeur de texte avec une prise en main difficile mais tout de même de nombreux développeurs l'utilisent.
+		<br>
+		<a href="https://www.gnu.org/software/emacs/"> Site internet officiel </a> (en anglais)
+	</p>
+
+    <h4>autre:</h4>
 	<h4>WordPad</h4>
 	<p>
 		<a href="http://windows.microsoft.com/fr-fr/windows7/products/features/wordpad">WordPad</a> est mis par défaut sous Windows. Il ne comprend ni correcteur orthographique ni gestion des styles. Il est bon pour écrire des lettres ou des textes courts mais n’est pas assez puissant pour de nombreuses tâches ou pour rédiger de longs rapports avec des graphiques ou de longs textes, tels des thèses ou des livres.
-	</p>
-
-	<h4>Geany</h4>
-	<p>
-		<a href="http://www.geany.org/"> Geany </a> est un éditeur de texte léger (utilisé par certains programmeurs) utilisant GTK+ et Scintilla et incluant les fonctions élémentaires d'un environnement de développement intégré. Il est pensé pour avoir peu de dépendances et démarrer rapidement, il est disponible pour plusieurs systèmes d'exploitation tel que Windows, Linux, Mac OS X3, BSD et Solaris. Il supporte, entre autres, les langages C/C++, Java, JavaScript, PHP, HTML, CSS, Python, Perl, Ruby, Pascal et Haskell.
 	</p>
 
 	<div>
---
+++ website/articles/article_ide.php
@@ -0,0 +1,4 @@
+<h4>Geany</h4>
+	<p>
+		<a href="http://www.geany.org/"> Geany </a> est un éditeur de texte léger (utilisé par certains programmeurs) utilisant GTK+ et Scintilla et incluant les fonctions élémentaires d'un environnement de développement intégré. Il est pensé pour avoir peu de dépendances et démarrer rapidement, il est disponible pour plusieurs systèmes d'exploitation tel que Windows, Linux, Mac OS X3, BSD et Solaris. Il supporte, entre autres, les langages C/C++, Java, JavaScript, PHP, HTML, CSS, Python, Perl, Ruby, Pascal et Haskell.
+	</p>
